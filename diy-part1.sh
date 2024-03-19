dff77506df7ffbc229362bb9eaea97891240eb6c
2253845067 <2253845067@qq.com>
2024-03-19 17:03:45 +0800

    更新

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,21 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 添加5G
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/Modem-Support
+
+# 添加风扇
+git clone --depth=1 https://github.com/2253845067/h69k-fanctrl package/h69k-fanctrl
+
+# small大佬常用OpenWrt软件包源码合集处理
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+
+# 下载openclash内核
+git clone --depth=1 https://github.com/2253845067/Openclash-core feeds/smpackage/luci-app-openclash/root/etc/openclash/core
