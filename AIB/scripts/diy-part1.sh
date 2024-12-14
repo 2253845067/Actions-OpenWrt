8b272c6b574bf2e9d89d43ab1305eafa7bdce7c5
2253845067 <2253845067@qq.com>
2024-12-14 14:07:21 +0800

    update

--- AIB/scripts/diy-part1.sh
+++ AIB/scripts/diy-part1.sh
@@ -20,13 +20,19 @@
 # 添加5G
 git clone --depth=1 https://github.com/Siriling/5G-Modem-Support package/Modem-Support
 
+# 添加风扇
+git clone --depth=1 https://github.com/JiaY-shi/fancontrol package/fancontrol
+
 # 添加插件库
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git modem https://github.com/yaoyue12548/modem_feeds.git;main' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/luci/packages/net/alist
 ./scripts/feeds install -a 
