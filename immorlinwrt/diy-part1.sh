7a39351750b1d1a70a10ca9c0670e65e1394d964
2253845067 <2253845067@qq.com>
2025-09-29 16:17:26 +0800

    update conf

--- immorlinwrt/diy-part1.sh
+++ immorlinwrt/diy-part1.sh
@@ -14,4 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+# echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
