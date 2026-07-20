fbd0ed4a5a2f3900f6c32823e9210e723f8f74bb
2253845067 <2253845067@qq.com>
2026-07-20 13:05:32 +0800

    Add Aurora themes and Bandix

--- immorlinwrt/diy-part1.sh
+++ immorlinwrt/diy-part1.sh
@@ -14,4 +14,17 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+
+# 添加 QModem 软件源
+echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+
+# 添加主题软件包
+mkdir -p package/yuqi-package/theme
+git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config.git package/yuqi-package/theme/luci-app-aurora-config
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora.git package/yuqi-package/theme/luci-theme-aurora
+git clone --depth=1 https://github.com/rchen14b/luci-theme-glass.git package/yuqi-package/theme/luci-theme-glass
+
+# 添加网络流量监控应用
+mkdir -p package/yuqi-package/others
+git clone --depth=1 https://github.com/timsaya/luci-app-bandix.git package/yuqi-package/others/luci-app-bandix
+git clone --depth=1 https://github.com/timsaya/openwrt-bandix.git package/yuqi-package/others/openwrt-bandix
